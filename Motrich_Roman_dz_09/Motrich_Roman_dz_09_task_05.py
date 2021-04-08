40ad9d6644d1fdf8160e9249397c3a9bac3e2b92
roman <roman.motrich@yandex.ru>
2021-04-08 04:09:55 +0300

    Пятая задача сделано

--- Motrich_Roman_dz_09/Motrich_Roman_dz_09_task_05.py
+++ Motrich_Roman_dz_09/Motrich_Roman_dz_09_task_05.py
@@ -19,11 +19,14 @@ SportCar, WorkCar, PoliceCar;
 полицейских автомобилей перед названием автомобиля
 должно идти слово POLICE;
 Для классов TownCar и WorkCar в методе get_status()
-рядом со значением скорости должна выводиться фраза "ПРЕВЫШЕНИЕ!", если скорость превышает 60 (TownCar) и 40 (WorkCar).
-Создайте по одному экземпляру каждого производного класса. В цикле из 10 итераций, для каждого автомобиля сделайте одно из случайных действий: go, stop, turn со случайными параметрами. После каждого действия показывайте статус автомобиля.
+рядом со значением скорости должна выводиться фраза
+"ПРЕВЫШЕНИЕ!", если скорость превышает 60 (TownCar) и 40 (WorkCar).
+Создайте по одному экземпляру каждого производного класса.
+В цикле из 10 итераций, для каждого автомобиля сделайте одно из
+случайных действий: go, stop, turn со случайными параметрами.
+После каждого действия показывайте статус автомобиля.
 '''
-
-
+import random
 
 
 class Car:
@@ -33,35 +36,60 @@ class Car:
         self.speed=0
         self.name=name
         self.turned='Химки'
+        self.speed_lim=None
 
 
     def go(self,speed):
         self.speed=speed
 
-    def stop():
+    def stop(self):
         self.speed=0
 
     def turn(self,turned):
-        pass
+        self.turned=turned
 
     def is_police(self):
         return self.polis_car
 
     def get_status(self):
         interpol='POLICE' if self.is_police() else ''
-        if speed> 0:
-            return f'{self.color}{self.name}движется со скоростью {self.speed} {self.turned}'
+
+        if self.speed> 0:
+            if self.speed_lim and self.speed>self.speed_lim:
+                warning='Превышение'
+            else:
+                warning =''# 'Скоростной режим соблюдён'
+            return f'{self.color} {self.name} движется со скоростью {self.speed} на {self.turned}, {warning} {interpol}'
         else:
-            return f'{self.color}{self.name} стоит'
+            return f'{self.color} {self.name} стоит {interpol}'
 
 class   TownCar(Car):
-    pass
+    def __init__(self, color, name):
+        self.color=color
+        self.polis_car=False
+        self.speed=100
+        self.name=name
+        self.turned='Химки'
+        self.speed_lim=60
 
 class   SportCar(Car):
-    pass
+    def __init__(self, color, name):
+        self.color=color
+        self.polis_car=False
+        self.speed=100
+        self.name=name
+        self.turned='Химки'
+        self.speed_lim=None
 
 class   WorkCar(Car):
-    pass
+    def __init__(self, color, name):
+        self.color=color
+        self.polis_car=False
+        self.speed=100
+        self.name=name
+        self.turned='Химки'
+        self.speed_lim=40
+
 
 class   PoliceCar(Car):
     def __init__(self, color, name):
@@ -70,3 +98,25 @@ class   PoliceCar(Car):
         self.speed=100
         self.name=name
         self.turned='Химки'
+        self.speed_lim = None
+
+
+
+
+cars=[TownCar('Белый','Мазда'),
+TownCar('Синий','Мицубик'),
+TownCar('Красный','Калина'),
+PoliceCar('Серый','УАЗ'),
+WorkCar('Оранжевый','Камаз')
+]
+
+for i in range(10):
+    for car in cars:
+        action=random.randint(1,3)
+        if action==1:
+            car.go(random.randint(0,100))
+        elif action==2:
+            car.turn(random.choice(['Запад','Север','Восток','Юг']))
+        elif action==3:
+            car.stop()
+        print(car.get_status())
