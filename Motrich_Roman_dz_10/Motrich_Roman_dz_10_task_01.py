7fbeea06ef1760431b216246e8d69f4124bd1b97
roman <roman.motrich@yandex.ru>
2021-04-09 07:25:43 +0300

    Решена задача 1

--- Motrich_Roman_dz_10/Motrich_Roman_dz_10_task_01.py
+++ Motrich_Roman_dz_10/Motrich_Roman_dz_10_task_01.py
@@ -37,25 +37,64 @@
 class Matrix:
     def __init__(self, input_matrix):
         if type(input_matrix)==list:
-            print('поступил список, not bad, проверка размерности')
-            matrix_h=len(input_matrix)
-            matrix_w=len(input_matrix[0])
+            self.matrix_h=len(input_matrix)
+            self.matrix_w=len(input_matrix[0])
             elem_type=type(input_matrix[0][0])
-            for string in input_matrix:
-                #проверка длины списков
-                if len(string)!= matrix_w:
-                    print('нашлась неправильная строка')
+
+            for string_m in input_matrix:
+
+                if len(string_m)!= self.matrix_w:
+                    raise ValueError('нашлась неправильная строка')
                 else:
-                    print('проверка типов')
-                    for i in string:
-                        if type(i)!= elem_type:
-                            print('Нашёлся неверный тип')
+
+                    for i in string_m:
+                        if type(i)!= int:
+                            raise ValueError('Нашёлся неверный тип')
                         else:
                             pass
         else:
-            print('NOT LIST - very bad')
+            raise ValueError('NOT LIST - very bad')
         self.matrix=input_matrix
+
+    def __add__(self, other):
+
+        sum_matrix = []
+
+        if self.matrix_w!= other.matrix_w or self.matrix_h!= other.matrix_h:
+            raise ValueError("Несоответствующая размерность")#else ''
+
+        for i in range(self.matrix_h):
+            tmp_list=[]
+            for k in range(self.matrix_w):
+                tmp_list.append(self.matrix[i][k]+self.matrix[i][k])
+            sum_matrix.append(tmp_list)
+
+        return Matrix(sum_matrix)
+
+    def __str__(self):
+        ret_srt=[]
+        for i in range(self.matrix_h):
+            for k in range(self.matrix_w):
+                ret_srt.append('{0:^3}'.format(self.matrix[i][k]))
+            ret_srt.append('\n')
+        return ' '.join(ret_srt)
+
+
+
+
+
 
 
 
 m=Matrix([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+n=Matrix([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+n2=Matrix([[1,2,3],[4,5,6],[7,8,9],[10,11,12],[4,5,6]])
+print(m)
+print(n)
+c=m+n
+print(c)
+print('-----------')
+print(m)
+print(n2)
+c=m+n2
+print(c)
